2d9a411be0be3a552f4bd703fda6cb01939923c0
tergvidas <tergvidas@gmail.com>
2017-03-31 20:54:13 +0300

    Update MenuScript.cs

--- puslapis/Parking simulator UNITY/Assets/MenuScript.cs
+++ puslapis/Parking simulator UNITY/Assets/MenuScript.cs
@@ -9,19 +9,6 @@ public class MenuScript : MonoBehaviour {
     public Canvas AboutMenu;
     public Canvas ScoreMenu;
 
-    //public Canvas ScoreMenu;
-    //Score menu buttons:
-   // public Button BackText;
-
-    //Main menu buttons:
-   /* public Button StartText;
-    public Button ScoreText;
-    public Button AboutText;
-    public Button SettingsText;*/
-
-   
-
-
 	// Use this for initialization
 	void Start () {
         MainMenu = MainMenu.GetComponent<Canvas>();
@@ -29,30 +16,15 @@ public class MenuScript : MonoBehaviour {
         AboutMenu = AboutMenu.GetComponent<Canvas>();
         ScoreMenu = ScoreMenu.GetComponent<Canvas>();
 
-
-        /* BackText = BackText.GetComponent<Button>();
-         StartText = StartText.GetComponent<Button>();
-         ScoreText = ScoreText.GetComponent<Button>();
-         AboutText = AboutText.GetComponent<Button>();
-         SettingsText = SettingsText.GetComponent<Button>();*/
-
         SettingsMenu.enabled = false;
         ScoreMenu.enabled = false;
         AboutMenu.enabled = false;
 
         MainMenu.enabled = true;
-        
-      //  BackText.enabled = false;
     }
 	
     public void ScorePress(){
         ScoreMenu.enabled = true;
-       // StartText.enabled = false;
-       // ScoreText.enabled = false;
-       // AboutText.enabled = false;
-      //  SettingsText.enabled = false;
-
-       // BackText.enabled = true;
 
         MainMenu.enabled = false;
         SettingsMenu.enabled = false;
@@ -82,12 +54,6 @@ public class MenuScript : MonoBehaviour {
     }
 
     public void BackPress(){
-        // StartText.enabled = false;
-        // ScoreText.enabled = false;
-        //AboutText.enabled = false;
-        // SettingsText.enabled = false;
-
-        // BackText.enabled = true;
 
         SettingsMenu.enabled = false;
         ScoreMenu.enabled = false;
@@ -96,25 +62,12 @@ public class MenuScript : MonoBehaviour {
         MainMenu.enabled = true;
     }
 
-  /*  public void NoPress()
-    {
-        SettingsMenu.enabled = false;
-        StartText.enabled = true;
-        ScoreText.enabled = true;
-        AboutText.enabled = true;
-        SettingsMenu.enabled = true;
-    }*/
-
     /*public void StartLevel()
     {
         Application.LoadLevel(1);
         
     }*/
-    /*public void ExitGame()
-    {
-       // Application.Quit();
-
-    }*/
+	
     // Update is called once per frame
     void Update () {
 		
