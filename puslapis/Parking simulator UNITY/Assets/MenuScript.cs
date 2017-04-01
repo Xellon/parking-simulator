953e800da12812f00f2b24bcbebf844b359f6921
tergvidas1 <tergvidas@gmail.com>
2017-04-01 15:26:19 +0300

    Pause menu
    Patvarkiau main menu script'a ir pridejau pause menu

--- puslapis/Parking simulator UNITY/Assets/MenuScript.cs
+++ puslapis/Parking simulator UNITY/Assets/MenuScript.cs
@@ -4,70 +4,61 @@ using UnityEngine;
 
 public class MenuScript : MonoBehaviour {
 
-    public Canvas MainMenu;
-    public Canvas SettingsMenu;
-    public Canvas AboutMenu;
-    public Canvas ScoreMenu;
+    public Canvas main_menu;
+    public Canvas settings_menu;
+    public Canvas about_menu;
+    public Canvas score_menu;
 
 	// Use this for initialization
 	void Start () {
-        MainMenu = MainMenu.GetComponent<Canvas>();
-        SettingsMenu= SettingsMenu.GetComponent<Canvas>();
-        AboutMenu = AboutMenu.GetComponent<Canvas>();
-        ScoreMenu = ScoreMenu.GetComponent<Canvas>();
+        main_menu = main_menu.GetComponent<Canvas>();
+        settings_menu= settings_menu.GetComponent<Canvas>();
+        about_menu = about_menu.GetComponent<Canvas>();
+        score_menu = score_menu.GetComponent<Canvas>();
 
-        SettingsMenu.enabled = false;
-        ScoreMenu.enabled = false;
-        AboutMenu.enabled = false;
+        settings_menu.enabled = false;
+        score_menu.enabled = false;
+        about_menu.enabled = false;
 
-        MainMenu.enabled = true;
+        main_menu.enabled = true;
+
     }
 	
-    public void ScorePress(){
-        ScoreMenu.enabled = true;
+    public void scorePress() {
+        score_menu.enabled = true;
 
-        MainMenu.enabled = false;
-        SettingsMenu.enabled = false;
-        AboutMenu.enabled = false;
+        main_menu.enabled = false;
+        settings_menu.enabled = false;
+        about_menu.enabled = false;
     }
 
-    public void SettingsPress(){
-        ScoreMenu.enabled = false;
-        MainMenu.enabled = false;
-        AboutMenu.enabled = false;
+    public void settingsPress() {
+        score_menu.enabled = false;
+        main_menu.enabled = false;
+        about_menu.enabled = false;
 
-        SettingsMenu.enabled = true;
+        settings_menu.enabled = true;
     }
 
-    public void AboutPress()
-    {
-        ScoreMenu.enabled = false;
-        MainMenu.enabled = false;
-        SettingsMenu.enabled = false;
+    public void aboutPress() {
+        score_menu.enabled = false;
+        main_menu.enabled = false;
+        settings_menu.enabled = false;
 
-        AboutMenu.enabled = true;
+        about_menu.enabled = true;
     }
 
-    public void PlayPress()
-    {
+    /*public void playPress() {
 
-    }
-
-    public void BackPress(){
-
-        SettingsMenu.enabled = false;
-        ScoreMenu.enabled = false;
-        AboutMenu.enabled = false;
-
-        MainMenu.enabled = true;
-    }
-
-    /*public void StartLevel()
-    {
-        Application.LoadLevel(1);
-        
     }*/
-	
+
+    public void backPress() {
+        settings_menu.enabled = false;
+        score_menu.enabled = false;
+        about_menu.enabled = false;
+
+        main_menu.enabled = true;
+    }
     // Update is called once per frame
     void Update () {
 		
