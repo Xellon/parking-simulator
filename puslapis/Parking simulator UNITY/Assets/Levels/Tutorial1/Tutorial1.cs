764f9b9dcec6b5dad14f652d92bf96f575b8fbb5
brassSyntax <leon.karpicz@gmail.com>
2017-04-16 20:34:38 +0300

    broken script to be fixed

---
+++ puslapis/Parking simulator UNITY/Assets/Levels/Tutorial1/Tutorial1.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Scenario:
+// (x) Load scene with camera from above (90 degrees?)
+// (x) Show a description of the tutorial + objectives and loop animation
+// (x) After you're done looking at animation press button to play
+// ( ) Replay animation from the menu
+// ( ) Win condition when in collision box
+
+public class Tutorial1 : MonoBehaviour {
+
+	public Transform vehicle;
+
+	private Transform player;
+	private Transform initialPosition;
+
+	void Start () {
+		player = vehicle.gameObject.transform.Find("FPSController");
+	}
+
+	void Update () {
+		if (Input.anyKey)
+		{
+			vehicle.gameObject.SetActive(true);
+
+			vehicle.Translate(initialPosition.position);
+			//vehicle.Rotate(initialPosition.rotation);
+
+			vehicle.GetComponent<Animation>().Stop();
+
+			this.gameObject.SetActive(false);
+		}
+		
+	}
+}
