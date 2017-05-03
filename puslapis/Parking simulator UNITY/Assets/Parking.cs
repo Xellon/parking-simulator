5083ac2ea50ba40cb417820a992e048e0b5f75b2
Xellon <eseviakov@gmail.com>
2017-05-03 11:16:12 +0300

    Pora pataisymų

--- puslapis/Parking simulator UNITY/Assets/Parking.cs
+++ puslapis/Parking simulator UNITY/Assets/Parking.cs
@@ -5,16 +5,17 @@ using UnityEngine.SceneManagement;
 
 public class Parking : MonoBehaviour {
     public GameObject masina;
-    public GameObject park;
+    private GameObject park;
 
     void Start() {
-
+        park = gameObject;
     }
     void Update() {
-        if (Input.GetKeyDown(KeyCode.KeypadEnter) && Variables.speed < 0.01 && Variables.speed > -0.01)
+        if (Input.GetKeyDown(KeyCode.Return) && Variables.speed < 0.01 && Variables.speed > -0.01)
             parkCheck();
     }
     private void parkCheck () {
+        Debug.Log(park.transform.position.x + " and " + masina.transform.position.x);
         if ((park.transform.position.x <= masina.transform.position.x + 0.3) && (park.transform.position.x >= masina.transform.position.x - 0.3) && 
             (park.transform.position.z <= masina.transform.position.z + 0.2) && (park.transform.position.z >= masina.transform.position.z - 0.2) &&
             (masina.transform.rotation.y == park.transform.rotation.y)  )
