b5668f02fb83b48a4429b43eb92aefd46d4a2cdc
brassSyntax <leon.karpicz@gmail.com>
2017-05-07 08:46:25 +0300

    sutvarkyta kamera, tutorial1.cs, parkavimo vieta

---
+++ puslapis/Parking simulator UNITY/Assets/CollideCam.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollideCam : MonoBehaviour
+{
+
+    private Rigidbody vehicle;
+
+    // Use this for initialization
+    void Start()
+    {
+        vehicle = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Triggered");
+    }
+}
