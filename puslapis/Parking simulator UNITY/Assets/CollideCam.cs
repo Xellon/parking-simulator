e81ad87506283930a42f0d0ffada70e0e236d74b
brassSyntax <leon.karpicz@gmail.com>
2017-05-10 09:06:34 +0300

    kamera

--- puslapis/Parking simulator UNITY/Assets/CollideCam.cs
+++ puslapis/Parking simulator UNITY/Assets/CollideCam.cs
@@ -6,11 +6,18 @@ public class CollideCam : MonoBehaviour
 {
 
     private Rigidbody vehicle;
+    private Camera collideCam;
 
     // Use this for initialization
     void Start()
     {
         vehicle = GetComponent<Rigidbody>();
+        if(vehicle == null)
+        {
+            Debug.Log("Vehicle objektas nenustatytas!");
+        }
+
+        collideCam = new Camera();
     }
 
     // Update is called once per frame
