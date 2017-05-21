2f4441ca7472ae40fc12d4af097094bfb70f32a8
Xellon <eseviakov@gmail.com>
2017-05-21 17:29:28 +0300

    Patvarkyta: level select, intro

--- puslapis/Parking simulator UNITY/Assets/UI/LevelChoices/LevelChoice.cs
+++ puslapis/Parking simulator UNITY/Assets/UI/LevelChoices/LevelChoice.cs
@@ -13,26 +13,24 @@ public class LevelChoice : MonoBehaviour {
     private Vector2 button_size;
 
     private int space = 50;
-    public int rows;
-    public int columns;
+    public int rows=3;
+    public int columns=8;
     //**************************************************************************************************************************************************************
 
     void Start () {
         template = GameObject.Find("ButtonLevel");
 
         button_size = new Vector2(template.GetComponent<RectTransform>().rect.width, template.GetComponent<RectTransform>().rect.height);
-        //rows = 3; //(int)(Screen.width / button_size.x + space);
-        //columns = 8; //(int)((Screen.height - 300) / button_size.y + space);
         float left = (Screen.width - columns * (button_size.x + space) + button_size.x) / 2;
         float top = (Screen.height - rows * (button_size.y + space) + button_size.x) / 2;
-
+        float height = Screen.height - top * 2;
         Debug.Log(Screen.width + "x" + Screen.height);
 
         int button_count = 0;
 
         for (int y = 0; y < rows; y++) {
             for (int x = 0; x < columns; x++) {
-                GameObject button = Instantiate(template, new Vector3(left + x * (button_size.x + space), top + y * (button_size.y + space)), new Quaternion(), gameObject.transform);
+                GameObject button = Instantiate(template, new Vector3(left + x * (button_size.x + space), top + height - y * (button_size.y + space)), new Quaternion(), gameObject.transform);
                 button.name = "ButtonLevel" + button_count;
                 button.transform.FindChild("Text").GetComponent<Text>().text = (button_count).ToString();
 
@@ -61,10 +59,6 @@ public class LevelChoice : MonoBehaviour {
                 button_count++;
             }
         }
-        background = transform.FindChild("Background");
-        background.position = new Vector3(Screen.width/2, Screen.height/2);
-        background.GetComponent<RectTransform>().sizeDelta = new Vector2(Screen.width, Screen.height);
-
     }
 
     private void Instantiate() {
@@ -80,8 +74,8 @@ public class LevelChoice : MonoBehaviour {
     //**************************************************************************************************************************************************************
 
     public void startLevel(int level_id) {
-        SceneManager.LoadScene(Variables.level_names[level_id], LoadSceneMode.Single);
         Variables.current_level = level_id;
+        SceneManager.LoadScene("LoadingScreen", LoadSceneMode.Single);
     }
 
     //**************************************************************************************************************************************************************
