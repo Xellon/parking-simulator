e069c84b4ee66290fc9992388ebce4b3cd38682c
brassSyntax <leon.karpicz@gmail.com>
2017-05-24 12:33:49 +0300

    Atsitrenkimo kamera

--- puslapis/Parking simulator UNITY/Assets/CollideCam.cs
+++ puslapis/Parking simulator UNITY/Assets/CollideCam.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CollideCam : MonoBehaviour
 {
@@ -8,7 +9,23 @@ public class CollideCam : MonoBehaviour
     private Rigidbody vehicle;
     private Camera collideCam;
 
-    // Use this for initialization
+    [Range(-5.0f, 5.0f)]
+    public float camHeight;
+
+    [Range(-5.0f, 5.0f)]
+    public float camRadius; 
+
+    [Range(0.0f, 5.0f)]
+    public float orbitSpeed;
+
+    public GameObject buttonPrefab;
+    private GameObject resetButton;
+
+    private float timeCount = 0;
+
+    private Vector3     initPos;
+    private Quaternion  initRot;
+
     void Start()
     {
         vehicle = GetComponent<Rigidbody>();
@@ -17,17 +34,65 @@ public class CollideCam : MonoBehaviour
             Debug.Log("Vehicle objektas nenustatytas!");
         }
 
-        collideCam = new Camera();
+        // initial position to reset the level
+        initPos = vehicle.transform.position;
+        initRot = vehicle.transform.rotation;
+
+        // cam creation
+        collideCam = new GameObject("CollideCamera").AddComponent<Camera>();
+        collideCam.transform.parent = vehicle.transform;
+        collideCam.gameObject.SetActive(false);
+
+        // button creation
+        resetButton = Instantiate(buttonPrefab);
+        resetButton.SetActive(false);
+        resetButton.transform.SetParent(GameObject.FindWithTag("MainUI").transform); // using MainUI tag set in inspector
+        resetButton.GetComponent<RectTransform>().anchoredPosition = new Vector3(0, -50, 0);
+        resetButton.GetComponent<Button>().onClick.AddListener(ResetLevel); // function to do on click
     }
 
-    // Update is called once per frame
     void Update()
     {
+        if(collideCam.gameObject.activeSelf)
+        {
+            timeCount += Time.deltaTime * orbitSpeed;
 
+            // orbit camera
+            collideCam.transform.position = new Vector3(
+                vehicle.transform.position.x + Mathf.Cos(timeCount) * camRadius,
+                vehicle.transform.position.y + camHeight,
+                vehicle.transform.position.z + Mathf.Sin(timeCount) * camRadius
+                );
+
+            // focus camera on the vehicle
+            collideCam.transform.LookAt(vehicle.transform);
+        }
+        
     }
 
     public void OnCollisionEnter(Collision other)
     {
-        Debug.Log("Triggered");
+        collideCam.gameObject.SetActive(true);
+        resetButton.SetActive(true);
+
+        Cursor.visible = true;
+
+        // freeze vehicle
+        vehicle.gameObject.GetComponent<Rigidbody>().isKinematic = true;
+    }
+
+    public void ResetLevel()
+    {
+        Cursor.visible = false;
+
+        // reset vehicle pos to original
+        vehicle.transform.SetPositionAndRotation(initPos, initRot);
+
+        resetButton.SetActive(false);
+        collideCam.gameObject.SetActive(false);
+        timeCount = 0;
+
+        // unfreeze vehicle
+        vehicle.gameObject.GetComponent<Rigidbody>().isKinematic = false;
     }
 }
